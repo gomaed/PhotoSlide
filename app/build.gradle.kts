ded94f71165018bb7ecf6bd5d5e860622964e255
gomaed <o0gomaed0o@gmail.com>
2026-04-08 23:54:33 +0200

    Bump versionCode to 6 and versionName to 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.gomaed.photoslide"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
